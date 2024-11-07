221a3ac357d5a426f12eab459db8f55f3955cce3
versatility519 <>
2024-11-07 18:45:45 +1000

    delete unnecessary files

--- apps/users/urls111.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import UserList, UserDetail
-
-urlpatterns = [
-    path('users', UserList.as_view(), name='user-list'),  # List and create users
-    path('users/<int:pk>', UserDetail.as_view(), name='user-detail'),  # Retrieve, update, delete user
-]
